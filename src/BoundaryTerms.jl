d71d3dd21d111f69c5a51b3d079545ea0216c883
alvestad10 <alvestad10@gmail.com>
2023-09-17 16:53:01 +0200

    Simplified boundary terms script and added 1+1D

--- src/BoundaryTerms.jl
+++ src/BoundaryTerms.jl
@@ -99,8 +99,8 @@ function Lc_x_x2_x0xt(u,H,m,λ,a,a_m1,gp1,gm1,κ)
     Lcx2[1:div(end,2)]     .= pre_fac*2*diag(KreT) .+ 2*(SjKTRe .* xR .- SjKTIm .* xI)
     Lcx2[div(end,2)+1:end] .= pre_fac*2*diag(KimT) .+ 2*(SjKTRe .* xI .+ SjKTIm .* xR)
     
-    Lcx2[1:1]     .= sum(pre_fac*2*diag(KreT) .+ 2*(SjKTRe .* xR .- SjKTIm .* xI))
-    Lcx2[div(end,2)+1:div(end,2)+1] .= sum(pre_fac*2*diag(KimT) .+ 2*(SjKTRe .* xI .+ SjKTIm .* xR))
+    # Lcx2[1:1]     .= sum(pre_fac*2*diag(KreT) .+ 2*(SjKTRe .* xR .- SjKTIm .* xI))
+    # Lcx2[div(end,2)+1:div(end,2)+1] .= sum(pre_fac*2*diag(KimT) .+ 2*(SjKTRe .* xI .+ SjKTIm .* xR))
     
 
     
@@ -113,85 +113,6 @@ end
 
 
 
-function Lc_x_x2_x0xt_1D(u,H,m,λ,a,a_m1,gp1,gm1,κ)
-
-    KRe,KIm = getK(H)
-
-    Lcx = zeros(size(u))
-    Lcx2 = zeros(size(u))
-    Lcx0xt = zeros(size(u))
-
-    xR =  @view u[1:div(end,2),:]
-    xI =  @view u[div(end,2)+1:end,:]
-    
-    #x = xR .+ im*xI
-
-    KreT = transpose(KRe)
-    KimT = transpose(KIm)
-
-    pre_fac = (1 / abs(a[1]))
-    
-    #_A[1:div(end,2)] 
-    ARe = - pre_fac .* 0.5 .* (
-        2. .* ( real.(a_m1) .* (xI .- xI[gp1])  .- imag.(a_m1 .- κ).*(xR .- xR[gp1]) ) ./ abs.(a_m1).^2
-    .- 2. .* ( real.(a)  .* (xI[gm1] .- xI) .- imag.(a .- κ)  .* (xR[gm1] .- xR) )./ abs.(a).^2
-    .- real.(a_m1 .+ a) .* (m .* xI .+ (1/6)*λ .* (-(xI.^3) .+ 3xI.*(xR.^2))) 
-    .- imag.(a_m1 .+ a) .* (m .* xR .+ (1/6)*λ .* (xR.^3 .- 3xR.*(xI.^2))) 
-    )     
-    
-    #_A[div(end,2)+1:end] 
-    AIm = pre_fac .* 0.5 .* (
-        2. .* (real.(a_m1).*(xR .- xR[gp1]) .+ imag.(a_m1 .- κ).*(xI .- xI[gp1])) ./ abs.(a_m1).^2
-    .- 2. .* (real.(a).*(xR[gm1] .- xR)   .+ imag.(a .- κ).*(xI[gm1] .- xI)) ./ abs.(a).^2
-    .- real.(a_m1 .+ a) .* (m.*xR .+ (1/6)*λ .* ((xR.^3) .- 3xR.*(xI.^2))) 
-    .+ imag.(a_m1 .+ a) .* (m.*xI .+ (1/6)*λ .* (-xI.^3 .+ 3xI.*(xR.^2))) 
-    )
-
-    #A = im*pre_fac*( 
-    #            (x .- x[gp1]) ./ a_m1 + (x .- x[gm1]) ./ a
-    #            .- (a .+ a_m1)/2 .* (m .* x .+ (λ/6) .* x.^3)
-    #    )
-    #ARe = real(A)
-    #AIm = imag(A)
-
-    SjKTRe = KRe*ARe .- KIm*AIm
-    SjKTIm = KRe*AIm .+ KIm*ARe
-    
-    #mul!(_B,K,[ARe ; AIm])
-    #_B = _A#transpose(_B)
-
-    #kReT = (p[1:div(end,2),:]*transpose(p[1:div(end,2),:]) 
-    #            - p[div(end,2)+1:end,:]*transpose(p[div(end,2)+1:end,:]))
-    #kImT = (p[1:div(end,2),:]*transpose(p[div(end,2)+1:end,:]) 
-    #            + p[div(end,2)+1:end,:]*transpose(p[1:div(end,2),:]))
-    
-    #ARe = @view _A[1:div(end,2)]
-    #AIm = @view _A[div(end,2)+1:end]
-    #SjKTRe = vec(transpose(ARe)*KreT .- transpose(AIm)*KimT)
-    #SjKTRe = @view _B[1:div(end,2)]#ARe #vec(transpose(ARe)*kReT .- transpose(AIm)*kImT)
-    #SjKTIm = vec(transpose(AIm)*KreT .+ transpose(ARe)*KimT)
-    #SjKTIm = @view _B[div(end,2) + 1:end]#AIm #vec(transpose(AIm)*kReT .+ transpose(ARe)*kImT)
-
-    Lcx[1:div(end,2)]     .= SjKTRe  #vec((transpose(ARe)*kReT .- transpose(AIm)*kImT))
-    Lcx[div(end,2)+1:end] .= SjKTIm #vec((transpose(AIm)*kReT .+ transpose(ARe)*kImT))
-    
-
-    
-    
-    Lcx2[1:div(end,2)]     .= pre_fac*2*diag(KreT) .+ 2*(SjKTRe .* xR .- SjKTIm .* xI)
-    Lcx2[div(end,2)+1:end] .= pre_fac*2*diag(KimT) .+ 2*(SjKTRe .* xI .+ SjKTIm .* xR)
-    
-    Lcx2[1:1]     .= sum(pre_fac*2*diag(KreT) .+ 2*(SjKTRe .* xR .- SjKTIm .* xI))
-    Lcx2[div(end,2)+1:div(end,2)+1] .= sum(pre_fac*2*diag(KimT) .+ 2*(SjKTRe .* xI .+ SjKTIm .* xR))
-    
-
-    
-    SjKj0Re = ARe.*view(KreT,:,1) .- AIm.*view(KimT,:,1)
-    SjKj0Im = AIm.*view(KreT,:,1) .+ ARe.*view(KimT,:,1)
-    Lcx0xt[1:div(end,2)]     .= pre_fac*(vec(KreT[1,:]) .+ vec(KreT[:,1])) .+ (SjKTRe .* xR[1] .- SjKTIm .* xI[1]) .+ (SjKj0Re .* xR .- SjKj0Im .* xI)
-    Lcx0xt[div(end,2)+1:end] .= pre_fac*(vec(KimT[1,:]) .+ vec(KimT[:,1])) .+ (SjKTRe .* xI[1] .+ SjKTIm .* xR[1]) .+ (SjKj0Re .* xI .+ SjKj0Im .* xR)
-    return [Lcx...,Lcx2...,Lcx0xt...]
-end
 
 
 
@@ -216,8 +137,152 @@ function getBoundaryTermsObservables(KP::KernelProblem{ScalarField{0}};kwargs...
 end
 
 
+function getBoundaryTermsObservables(KP::KernelProblem{ScalarField{1}};kwargs...)
 
+    @unpack model, kernel = KP
+    @unpack m, λ, contour, n_steps, as = model
+    @unpack a, t_steps = contour
+    @unpack H = kernel
 
+    _KRe, _KIm = getK(kernel)
+    KRe = Matrix(_KRe)
+    KIm = Matrix(_KIm)
+
+    KRe_tensor = reshape(transpose(KRe),t_steps,n_steps,t_steps,n_steps)
+    KIm_tensor = reshape(transpose(KIm),t_steps,n_steps,t_steps,n_steps)
+
+    diagKRe = diag(reshape(mean(KRe_tensor,dims=(2,4)),t_steps,t_steps)) ./ n_steps
+    diagKIm = diag(reshape(mean(KIm_tensor,dims=(2,4)),t_steps,t_steps)) ./ n_steps
+
+    use_GPU = false
+    if use_GPU
+        KRe = CuArray(KRe)
+        KIm = CuArray(KIm)
+    end
+    
+
+    gm1=vcat([t_steps],1:t_steps-1)
+    gp1=vcat(2:t_steps,[1])
+
+    gsm1=vcat([n_steps],1:n_steps-1)
+    gsp1=vcat(2:n_steps,[1])
+    
+    a_m1 = a[gm1]
+    as_prefac_re = 1 / (as^2)
+    as_prefac_im = 1/ (as^2) 
+
+    one_over_a = a.^(-1)
+    one_over_a_Re = real(one_over_a) #hcat([real(one_over_a) for i in 1:n_steps]...)
+    one_over_a_Im = imag(one_over_a) #hcat([imag(one_over_a) for i in 1:n_steps]...)
+    one_over_a_m1 = a_m1.^(-1)
+    one_over_a_m1_Re = real(one_over_a_m1) #hcat([real(one_over_a_m1) for i in 1:n_steps]...)
+    one_over_a_m1_Im = imag(one_over_a_m1) #hcat([imag(one_over_a_m1) for i in 1:n_steps]...)
+    
+    V_pre_fac = (a + a_m1)/2
+    V_pre_fac_Re = real(V_pre_fac) #hcat([real(V_pre_fac) for i in 1:n_steps]...)
+    V_pre_fac_Im = imag(V_pre_fac) #hcat([imag(V_pre_fac) for i in 1:n_steps]...)
+
+    pre_fac = (1. / abs(a[1]))
+    #im_pre_fac = im*pre_fac
+
+    isIdentity = all(diag(H) .- 1. .== 0.0)
+
+    LcO(u) = begin
+    
+        @unpack m, λ, contour, n_steps, as = model
+        @unpack t_steps = contour
+    
+        Lcx = zeros(2*t_steps)
+        Lcx2 = zeros(2*t_steps)
+        Lcx0xt = zeros(2*t_steps)
+    
+        _uRe = @view u[1:t_steps*n_steps]
+        _uIm = @view u[t_steps*n_steps + 1:end]
+
+        uRe = reshape(_uRe,t_steps,n_steps)
+        uIm = reshape(_uIm,t_steps,n_steps)
+        ARe = zeros(t_steps,n_steps)
+        AIm = zeros(t_steps,n_steps)
+
+        uRetm1 = @view uRe[gm1,:]
+        uRetp1 = @view uRe[gp1,:]
+        uResm1 = @view uRe[1:t_steps,gsm1]
+        uResp1 = @view uRe[1:t_steps,gsp1]
+        
+        uImtm1 = @view uIm[gm1,:]
+        uImtp1 = @view uIm[gp1,:]
+        uImsm1 = @view uIm[1:t_steps,gsm1]
+        uImsp1 = @view uIm[1:t_steps,gsp1]
+        
+        VRe = @. m * uRe + (λ/6) * (uRe^3 - 3*uRe*uIm^2)
+        VIm = @. m * uIm - (λ/6) * (uIm^3 - 3*uIm*uRe^2)
+ 
+
+        @inbounds for I in CartesianIndices((t_steps,n_steps))
+            i,j = Tuple(I)
+            ARe[i,j] = - as * pre_fac * (
+                    (uRe[i,j] - uRetm1[i,j])*one_over_a_m1_Im[i]
+                + (uIm[i,j] - uImtm1[i,j])*one_over_a_m1_Re[i]  
+                + (uRe[i,j] - uRetp1[i,j])*one_over_a_Im[i] 
+                + (uIm[i,j] - uImtp1[i,j])*one_over_a_Re[i]
+
+                - V_pre_fac_Im[i] * ( (2uRe[i,j] - uResm1[i,j] - uResp1[i,j])*as_prefac_im + 
+                                        VRe[i,j])
+                - V_pre_fac_Re[i] * ( (2uIm[i,j] - uImsm1[i,j] - uImsp1[i,j])*as_prefac_re + 
+                                        VIm[i,j])
+                )
+            
+            AIm[i,j] = as * pre_fac * (
+                    (uRe[i,j] - uRetm1[i,j])*one_over_a_m1_Re[i] 
+                - (uIm[i,j] - uImtm1[i,j])*one_over_a_m1_Im[i]  
+                + (uRe[i,j] - uRetp1[i,j])*one_over_a_Re[i] 
+                - (uIm[i,j] - uImtp1[i,j])*one_over_a_Im[i]
+        
+                - V_pre_fac_Re[i] * ( (2uRe[i,j] - uResm1[i,j] - uResp1[i,j])*as_prefac_re + 
+                                        VRe[i,j])
+                + V_pre_fac_Im[i] * ( (2uIm[i,j] - uImsm1[i,j] - uImsp1[i,j])*as_prefac_im +
+                                    VIm[i,j])
+                )
+        end
+
+        if isIdentity
+            SjKTRe = vec(ARe)
+            SjKTIm = vec(AIm)
+        elseif use_GPU
+            _ARe = CuArray(vec(ARe))
+            _AIm = CuArray(vec(AIm))
+            
+            SjKTRe = Array(KRe*_ARe - KIm*_AIm)
+            SjKTIm = Array(KIm*_ARe + KRe*_AIm)
+        else
+            _ARe = vec(ARe)
+            _AIm = vec(AIm)
+            KReARe = KRe*_ARe
+            KImAIm = KIm*_AIm
+            KReAIm = KRe*_AIm
+            KImARe = KIm*_ARe
+
+            SjKTRe = KReARe - KImAIm
+            SjKTIm = KImARe + KReAIm
+        end
+
+        SjKTRe = mean(reshape(SjKTRe,t_steps,n_steps), dims=2)
+        SjKTIm = mean(reshape(SjKTIm,t_steps,n_steps), dims=2)
+        
+        Lcx[1:div(end,2)]     .= SjKTRe #mean(SjKTRe,dims=2)  #vec((transpose(ARe)*kReT .- transpose(AIm)*kImT))
+        Lcx[div(end,2)+1:end] .= SjKTIm #mean(SjKTIm,dims=2) #vec((transpose(AIm)*kReT .+ transpose(ARe)*kImT))
+        
+        m_uRe = mean(uRe,dims=2)
+        m_uIm = mean(uIm,dims=2)
+
+        Lcx2[1:div(end,2)]     .= pre_fac*2*diagKRe .+ 2*(SjKTRe .* m_uRe .- SjKTIm .* m_uIm) ./ n_steps
+        Lcx2[div(end,2)+1:end] .= pre_fac*2*diagKIm .+ 2*(SjKTRe .* m_uIm .+ SjKTIm .* m_uRe) ./ n_steps
+        
+        return [Lcx...,Lcx2...,Lcx0xt...]
+    end
+    
+    return LcO, nothing#L2cO
+end
 
 
 
@@ -235,127 +300,52 @@ end
 
 
 
-
-
-
-function calcBoundaryTerms(sol,BT::BoundaryTerms{MType,YT,L,L2};T=Float64, witherror=true,box=false) where {MType <: Model,YT,L,L2}
-    @unpack Ys, Xs = BT
+function calcBoundaryTerms(sol,BT::BoundaryTerms{MType,YT,L,L2}) where {MType <: Model,YT,L,L2}
+    
+    @unpack Ys = BT
     NTr = length(sol)
 
-    realDir = !isnothing(Xs)
-    d = realDir ? 4 : 3
-
+    d = 3
     NObs = 6*BT.model.contour.t_steps
 
-    if realDir
-        LOs = zeros(T,NObs,length(Ys),length(Xs),NTr)
-        L2Os = (L2 == Nothing) ? nothing : zeros(Float64,NObs,length(Ys),length(Xs),NTr)
-    else
-        LOs = zeros(T,NObs,length(Ys),NTr)
-        L2Os = (L2 == Nothing) ? nothing : zeros(Float64,NObs,length(Ys),NTr)
-    end
+    LOs = zeros(T,NObs,length(Ys),NTr)
+    LOs_err = zeros(T,NObs,length(Ys),NTr)
 
     Threads.@threads for tr in 1:NTr
     #for tr in 1:NTr
 
         N = size(sol[tr])[2]
         trVs = zeros(T,NObs,N)
-        trV2s = (L2 == Nothing) ? nothing : zeros(T,NObs,N)
         
         @inbounds @simd for i in 1:N
             tri = @view sol[tr][:,i]
             trVs[:,i] = BT.L_CO(tri)
-
-            if L2 != Nothing
-                trV2s[:,i] = BT.L2_CO(tri)
-            end
         end
         
 
         ### TODO: This needs to be abstracted away to the specific model
-        if length(sol[tr][:,1]) > 2
-            if realDir || box
-                XX = @view sol[tr][1:div(end,2),:]
-                YY = @view sol[tr][div(end,2)+1:end,:]
-                imX = [sum(abs.(XX.^2 .- YY.^2),dims=1) ; sum(abs.(2 .* XX .* YY),dims=1)]
-                #imX = [maximum(abs.(XX),dims=1) ; maximum(abs.(YY),dims=1)]
-            else
-                imX = maximum(abs.(@view sol[tr][div(end,2)+1:end,:]),dims=1)
-            end
-        else
-            #imX = maximum(abs.(sol[tr]),dims=1)
-            if realDir || box
-                imX = abs.(sol[tr])
-            else
-                imX = abs.(@view sol[tr][2,:])
-            end
-        end
+        XX = @view sol[tr][1:div(end,2),:]
+        YY = @view sol[tr][div(end,2)+1:end,:]
+        #imX = [sum(abs.(XX.^2 .- YY.^2),dims=1) ; sum(abs.(2 .* XX .* YY),dims=1)]
+        imX = [maximum(abs.(XX),dims=1) ; maximum(abs.(YY),dims=1)]
+        #imX = [mean(abs.(XX),dims=1) ; mean(abs.(YY),dims=1)]
+    
 
-        if realDir
-            trLOs = zeros(T,NObs,length(Ys),length(Xs))
-            trL2Os = (L2 == Nothing) ? nothing : zeros(Float64,NObs,length(Ys),length(Xs))
-        else
-            trLOs = zeros(T,NObs,length(Ys))
-            trL2Os = (L2 == Nothing) ? nothing : zeros(Float64,NObs,length(Ys))
-        end
+        trLOs = zeros(T,NObs,length(Ys))
+        err_trLOs = zeros(T,NObs,length(Ys))
         for i in eachindex(Ys)
-            if realDir
-                for j in eachindex(Xs)
-                    Hinx = @. (imX[2,:] .<= Ys[i]) .& (imX[1,:] .<= Xs[j])
-                    H = @view trVs[:,Hinx]
-                    trLOs[:,i,j] = sum(H,dims=2)/N
-                    if L2 != Nothing
-                        H2 = @view trV2s[:,Hinx]
-                        trL2Os[:,i,j] = sum(H2,dims=2)/N
-                    end
-                end
-            elseif box
-                Hinx = @. (imX[2,:] .<= Ys[i]) .& (imX[1,:] .<= Ys[i])
-                H = @view trVs[:,Hinx]
-                trLOs[:,i] = sum(H,dims=2)/N
-                if L2 != Nothing
-                    H2 = @view trV2s[:,Hinx]
-                    trL2Os[:,i] = sum(H2,dims=2)/N
-                end
-            else 
-                Hinx = @. (imX[:] .<= Ys[i])
-                H = @view trVs[:,Hinx]
-                trLOs[:,i] = sum(H,dims=2)/N
-                if L2 != Nothing
-                    H2 = @view trV2s[:,imX .<= Ys[i]]
-                    trL2Os[:,i] = sum(H2,dims=2)/N
-                end
-            end
+            Hinx = @. (imX[2,:] .<= Ys[i]) .& (imX[1,:] .<= Ys[i])
+            H = @view trVs[:,Hinx]
+            trLOs[:,i] = sum(H,dims=2)/N
+            err_trLOs[:,i] = sqrt.(sum(H.^2,dims=2)/N .- trLOs[:,i].^2) ./ sqrt(N)
         end
-        LOs[map((i) -> :,1:(d-1))...,tr] .= trLOs
-        if L2 != Nothing
-            L2Os[map((i) -> :,1:(d-1))...,tr] .= trL2Os
-        end
+        LOs[:,:,tr] .= trLOs
+        LOs_err[:,:,tr] .= err_trLOs
     end
-    if L2 == Nothing
-        if witherror
-            return dropdims(mean(LOs,dims=d),dims=d) .± dropdims(std(LOs,dims=d),dims=d) ./ sqrt(NTr), 
-                    nothing
-        else
-            return dropdims(mean(LOs,dims=d),dims=d),# .± dropdims(std(LOs,dims=d),dims=d) ./ sqrt(NTr), 
-                    nothing
-        end
-    else
-        return dropdims(mean(LOs,dims=d),dims=d) .± dropdims(std(LOs,dims=d),dims=d) ./ sqrt(NTr),
-               dropdims(mean(L2Os,dims=d),dims=d) .± dropdims(std(L2Os,dims=d),dims=d) ./ sqrt(NTr)
-    end
-end
+    
+    return mean(LOs,dims=3)  .± sqrt.(sum(LOs_err.^2,dims=3))
+    #return dropdims(mean(LOs,dims=d),dims=d) .± dropdims(std(LOs,dims=d),dims=d) ./ sqrt(NTr)
 
-function calcBoundaryTermsCorrections(sol,BT::BoundaryTerms,lims::Tuple{T, T},lims2::Tuple{T, T}) where {T <: Real}
-    isnothing(BT.L2_CO) && return 0.
-    L_CO, L2_CO = calcBoundaryTerms(sol,BT)
-
-    # Interpolate the boundary value
-    Yinx = findall((x) -> lims[1] <= x <= lims[2], BT.Ys)
-    Yinx2 = findall((x) -> lims2[1] <= x <= lims2[2], BT.Ys)
-    B1 = weightedmean(L_CO[Yinx])
-    B2 = weightedmean(L2_CO[Yinx2])
-    return B1, B2
 end
 
 
