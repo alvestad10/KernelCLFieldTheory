7d23590382afcc98ee2f4d2ec43dea213bcb57b5
alvestad10 <alvestad10@gmail.com>
2023-09-20 09:08:19 +0200

    Updating the script.jl

--- PaperPlots/CanonicalSK/script.jl
+++ PaperPlots/CanonicalSK/script.jl
@@ -1,12 +1,23 @@
 using KernelCLFieldTheory
 using JLD2
 using Plots
+using LaTeXStrings
 using Statistics, LinearAlgebra
+
+
+############################################################################
+##  Load and Save the results
+############################################################################
+DIR = "PaperPlots/CanonicalSK/results/RT_10_at_01_as_02/run1"
+if !isdir(DIR)
+    mkpath(DIR)
+end
+############################################################################
 
 
 Base.ENV["GKSwstype"]="nul"
 
-M = ScalarField_from_at(D=0,m=1.0,λ=24.,RT=1.6,β=0.8,at=0.2,n_steps=8,as=0.2,
+M = ScalarField_from_at(D=1,m=1.0,λ=1.,RT=1.0,β=0.4,at=0.1,n_steps=8,as=0.2,
                         Δβ = 0.5                
                         #ΔE = 0.05              
 )
@@ -39,22 +50,23 @@ KP = KernelCLFieldTheory.KernelProblem(M);
 #    previous run
 ############################################################################
 scheme = ImplicitEM()
-RS_train = RunSetup(tspan=5,
+RS_train = RunSetup(tspan=3,
                     saveat=0.005,
-                    tspan_thermalization=400,  
+                    tspan_thermalization=50,  
+                    dtmax=1e-4, abstol=1e-4, reltol=1e-4,
                     scheme=scheme
 )
 
 # The validation setup
-RS_val = RunSetup(tspan=5000, 
-                    tspan_thermalization=1000, 
+RS_val = RunSetup(tspan=1, 
+                    tspan_thermalization=1, 
                     scheme=scheme
 )
 
 # The testing setup
-RS_test = RunSetup(tspan=500, 
+RS_test = RunSetup(tspan=1000, 
                     tspan_thermalization=100, 
-                    scheme=scheme, dtmax=1e-5, # abstol=1e-5, reltol=1e-5,
+                    scheme=scheme, dtmax=1e-4, abstol=1e-4, reltol=1e-4,
                     NTr=10
 )
 
@@ -76,8 +88,8 @@ RS_test = RunSetup(tspan=500,
 #    Kernel Optimization setup
 ############################################################################
 opt = ADAM(0.001, (0.5, 0.9))
-epochs = 5
-runs_pr_epoch = 40
+epochs = 10
+runs_pr_epoch = 50
 n_gradient_pr_run = 1
 
 
@@ -137,22 +149,38 @@ LK = LearnKernel(KP,RS_train;RS_val=RS_val,
                  runs_pr_epoch=runs_pr_epoch,
                  n_gradient_pr_run = n_gradient_pr_run,
                  cb=cb)
-bestKernel, bestl, KP, u0s = learnKernel(LK; reset_u0s=true)
+bestKernel, bestl, KP, u0s = learnKernel(LK; reset_u0s=true, validation=false)
 
-####
-# Updating LK with new KP just construct a new LK above.
-####
+########## SAVE
+jldsave(joinpath(DIR,"KP.jld2"), KP=KP)
+
+############################################################################
+#    Plot the loss history
+############################################################################
+lplot_train = plot(lhistory_train[:L],label="L", yaxis=:log)
+lsymplot_train = plot(lhistory_train[:LSym],label="LSym", yaxis=:log)
+
+#lplot_val = plot(lhistory_val[:L],label="L", yaxis=:log)
+#lsymplot_val = plot(lhistory_val[:LSym],label="LSym", yaxis=:log)
+
+
+########## SAVE
+jldsave(joinpath(DIR,"lhistory.jld2"), lhistory_train=lhistory_train, lhistory_val=lhistory_val)
+KernelCLFieldTheory.savefig(lplot_train,joinpath(DIR,"lplot_train.pdf"))
+KernelCLFieldTheory.savefig(lsymplot_train,joinpath(DIR,"lsymplot_train.pdf"))
+#KernelCLFieldTheory.savefig(lplot_val,joinpath(DIR,"lplot_val.pdf"))
+#KernelCLFieldTheory.savefig(lsymplot_val,joinpath(DIR,"lsymplot_val.pdf"))
 
 ############################################################################
 #    Testing optimized kernel
 ############################################################################
-bestKP = KernelCLFieldTheory.KernelProblem(M, kernel=bestKernel);
-@time sol_bestKP = run_simulation(bestKP,RS_test; u0=u0s);
+# bestKP = KernelCLFieldTheory.KernelProblem(M, kernel=bestKernel);
+# @time sol_bestKP = run_simulation(bestKP,RS_test; u0=u0s);
 
-fig_SK_bestKP = plotSKContour(bestKP,sol_bestKP)
-fig_FW_bestKP = plotFWContour(bestKP,sol_bestKP)
+# fig_SK_bestKP = plotSKContour(bestKP,sol_bestKP)
+# fig_FW_bestKP = plotFWContour(bestKP,sol_bestKP)
 
-obs_bestKP = KernelCLFieldTheory.calc_obs(KP,sol_bestKP)
+# obs_bestKP = KernelCLFieldTheory.calc_obs(KP,sol_bestKP)
 
 #u0s = [tr[:,end] for tr in sol_KP]
 @time sol_KP = run_simulation(KP,RS_test)#; u0=u0s);
@@ -168,14 +196,18 @@ obs_KP = KernelCLFieldTheory.calc_obs(KP,sol_KP)
 
 # obs_new_u0Init = KernelCLFieldTheory.calc_obs(bestKP,sol_new_u0Init)
 
-############################################################################
-#    Plot the loss history
-############################################################################
-lplot_train = plot(lhistory_train[:L],label="L", yaxis=:log)
-lsymplot_train = plot(lhistory_train[:LSym],label="LSym", yaxis=:log)
 
-lplot_val = plot(lhistory_val[:L],label="L", yaxis=:log)
-lsymplot_val = plot(lhistory_val[:LSym],label="LSym", yaxis=:log)
+########## SAVE
+jldsave(joinpath(DIR,"sol_KP.jld2"), sol_KP=sol_KP)
+jldsave(joinpath(DIR,"obs_KP.jld2"), obs_KP=obs_KP)
+
+KernelCLFieldTheory.savefig(fig_SK_KP,joinpath(DIR,"fig_SK_KP.pdf"))
+KernelCLFieldTheory.savefig(fig_FW_KP,joinpath(DIR,"fig_FW_KP.pdf"))
+
+
+
+
+
 
 
 ############################################################################
@@ -183,58 +215,56 @@ lsymplot_val = plot(lhistory_val[:LSym],label="LSym", yaxis=:log)
 ############################################################################
 
 
-BT = getBoundaryTerms(bestKP;Ys=collect(0:1:80))#,Xs=collect(0:0.1:8))
-
-B1, B2 = calcBoundaryTerms(sol_bestKP,BT;T=Float64, witherror=true,box=true)
+Ys = collect(0:0.2:15)
+BT = getBoundaryTerms(KP;Ys=Ys)#,Xs=collect(0:0.1:8))
+@time B1 = calcBoundaryTerms(sol_KP,BT)
 
 begin
     observables = ["xRe", "xIm", "x2Re", "x2Im", "corr0tRe", "corr0tIm"]
     t_steps = KP.model.contour.t_steps
 
-    fig = plot() 
+    fig_BT = plot(xlabel=L"\Omega", ylabel=L"B_1(\Omega)";KernelCLFieldTheory.plot_setup(:topright)...)
     for i in eachindex(observables)
-        if i < 3  || i > 4
+        if i > 4 || i < 3
             continue
         end
         _BT_mean = vec(mean(B1[(i-1)*t_steps .+ (1:t_steps),:],dims=1))
-        plot!(fig,_BT_mean, label=observables[i])
+        scatter!(fig_BT,Ys,_BT_mean, label=observables[i];KernelCLFieldTheory.markers_dict(i)...)
     end
-    fig
+    hline!(fig_BT,[0.0];label=false,KernelCLFieldTheory.solution_line_dict()...)
+    fig_BT
 end
 
+jldsave(joinpath(DIR,"BTs.jld2"), BT=BT)
+jldsave(joinpath(DIR,"B1.jld2"), B1=B1)
+
+KernelCLFieldTheory.savefig(fig_BT,joinpath(DIR,"fig_BT.pdf"))
 
 
 
-############################################################################
-##  Load and Save the results
-############################################################################
-DIR = "PaperPlots/CanonicalSK/results/RT_44_at_01_as_02/"
-if !isdir(DIR)
-    mkdir(DIR)
-end
 ############################################################################
 
 ### SAVE
-jldsave(joinpath(DIR,"lhistory_3.jld2"), lhistory_train=lhistory_train, lhistory_val=lhistory_val)
-KernelCLFieldTheory.savefig(lplot_train,joinpath(DIR,"lplot_train.pdf"))
-KernelCLFieldTheory.savefig(lsymplot_train,joinpath(DIR,"lsymplot_train.pdf"))
-KernelCLFieldTheory.savefig(lplot_val,joinpath(DIR,"lplot_val.pdf"))
-KernelCLFieldTheory.savefig(lsymplot_val,joinpath(DIR,"lsymplot_val.pdf"))
+# jldsave(joinpath(DIR,"lhistory.jld2"), lhistory_train=lhistory_train, lhistory_val=lhistory_val)
+# KernelCLFieldTheory.savefig(lplot_train,joinpath(DIR,"lplot_train.pdf"))
+# KernelCLFieldTheory.savefig(lsymplot_train,joinpath(DIR,"lsymplot_train.pdf"))
+# KernelCLFieldTheory.savefig(lplot_val,joinpath(DIR,"lplot_val.pdf"))
+# KernelCLFieldTheory.savefig(lsymplot_val,joinpath(DIR,"lsymplot_val.pdf"))
 
-jldsave(joinpath(DIR,"sol_bestKP.jld2"), sol_bestKP=sol_bestKP)
-jldsave(joinpath(DIR,"obs_bestKP.jld2"), obs_bestKP=obs_bestKP)
-jldsave(joinpath(DIR,"bestKP.jld2"), bestKP=bestKP)
+# jldsave(joinpath(DIR,"sol_bestKP.jld2"), sol_bestKP=sol_bestKP)
+# jldsave(joinpath(DIR,"obs_bestKP.jld2"), obs_bestKP=obs_bestKP)
+# jldsave(joinpath(DIR,"bestKP.jld2"), bestKP=bestKP)
 
-KernelCLFieldTheory.savefig(fig_SK_bestKP,joinpath(DIR,"fig_SK_bestKP.pdf"))
-KernelCLFieldTheory.savefig(fig_FW_bestKP,joinpath(DIR,"fig_FW_bestKP.pdf"))
+# KernelCLFieldTheory.savefig(fig_SK_bestKP,joinpath(DIR,"fig_SK_bestKP.pdf"))
+# KernelCLFieldTheory.savefig(fig_FW_bestKP,joinpath(DIR,"fig_FW_bestKP.pdf"))
 
 
-jldsave(joinpath(DIR,"sol_KP.jld2"), sol_KP=sol_KP)
-jldsave(joinpath(DIR,"obs_KP.jld2"), obs_KP=obs_KP)
-jldsave(joinpath(DIR,"KP.jld2"), KP=KP)
+# jldsave(joinpath(DIR,"sol_KP.jld2"), sol_KP=sol_KP)
+# jldsave(joinpath(DIR,"obs_KP.jld2"), obs_KP=obs_KP)
+# jldsave(joinpath(DIR,"KP.jld2"), KP=KP)
 
-KernelCLFieldTheory.savefig(fig_SK_KP,joinpath(DIR,"fig_SK_KP.pdf"))
-KernelCLFieldTheory.savefig(fig_FW_KP,joinpath(DIR,"fig_FW_KP.pdf"))
+# KernelCLFieldTheory.savefig(fig_SK_KP,joinpath(DIR,"fig_SK_KP.pdf"))
+# KernelCLFieldTheory.savefig(fig_FW_KP,joinpath(DIR,"fig_FW_KP.pdf"))
 
 
 
